dda3939ea646bc21915dddef112503e932ea417a
Nayan <geeknayan@gmail.com>
2026-05-10 23:45:01 +0530

    kjdsajk

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
 #include <iot_core/IIotCoreApp.h>
+#include "../device/IDeviceCollection.h"
+
+IIoTCoreAppPtr iotCoreApp;
 
 /* @Autowired */
-IIoTCoreAppPtr iotCoreApp;
+IDeviceCollectionPtr deviceCollection;
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("dsds jdeoang-un");
   iotCoreApp = Implementation<IIoTCoreApp>::type::GetInstance();
   iotCoreApp->Start();
 }
 
 void loop() {
+  //Serial.println("ddd joang-un");
   iotCoreApp->Loop();
+  deviceCollection->RefreshAllDevices();
+  delay(1000);
 }
+
